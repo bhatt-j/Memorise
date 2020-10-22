560677fae119fa835c017361c8df39767694b8fd
drashti1905 <drashtisaglani1905@gmail.com>
2020-10-23 00:48:45 +0530

    app....

---
+++ app/src/main/java/com/example/memorise/Hard_level.java
@@ -0,0 +1,14 @@
+package com.example.memorise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hard_level extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hard_level);
+    }
+}
---
+++ app/src/main/java/com/example/memorise/Medium_level.java
@@ -0,0 +1,13 @@
+package com.example.memorise;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Medium_level extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medium_level);
+    }
+}
